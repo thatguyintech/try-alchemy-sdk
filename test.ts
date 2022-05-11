85cfd1a6db358c5776466fd52c31f038764e6aac
ALBERT <thatguyintech@gmail.com>
2022-05-11 09:59:59 -0700

    import the entire sdk at once for simplicity

--- test.ts
+++ test.ts
@@ -1,15 +1,14 @@
-import {
-    AlchemyConfig, Network, initializeAlchemy, getNftsForOwner } from 'alchemy-sdk';
+import * as sdk from 'alchemy-sdk';
 import 'dotenv/config';
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
-const settings: AlchemyConfig = {
+const settings: sdk.AlchemyConfig = {
   apiKey: process.env.API_KEY,
-  network: Network.ETH_MAINNET,
+  network: sdk.Network.ETH_MAINNET,
   maxRetries: 10
 };
 
-const alchemy = initializeAlchemy(settings);
-getNftsForOwner(alchemy, '0xshah.eth').then(thing => {
+const alchemy = sdk.initializeAlchemy(settings);
+sdk.getNftsForOwner(alchemy, '0xshah.eth').then(thing => {
   console.log(thing);
 });
